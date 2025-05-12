cfde1c6f0a0afd93cb6b221236a45f1f2eacd8a3
Matelz <49626198+Matelz@users.noreply.github.com>
2025-05-11 22:55:34 -0300

    hotfix: add missing unit tests and fixed imports

--- src/app/environments.py
+++ src/app/environments.py
@@ -2,9 +2,17 @@
 from enum import Enum
 import os
 
+<<<<<<< Updated upstream
 from .errors.environment_errors import EnvironmentNotFound
 
 from .repo.item_repository_interface import IItemRepository
+=======
+from src.app.repo.account_repository_interface import IAccountRepository
+from src.app.repo.transaction_history_repository_interface import ITransactionHistoryRepository
+from src.app.repo.transaction_repository_interface import ITransactionRepository
+
+from src.app.errors.environment_errors import EnvironmentNotFound
+>>>>>>> Stashed changes
 
 
 class STAGE(Enum):
@@ -33,15 +41,6 @@ class Environments:
             self._configure_local()
 
         self.stage = STAGE[os.environ.get("STAGE")]
-
-    @staticmethod
-    def get_item_repo() -> IItemRepository:
-        if Environments.get_envs().stage == STAGE.TEST:
-            from .repo.item_repository_mock import ItemRepositoryMock
-            return ItemRepositoryMock
-        # use "elif" conditional to add other stages
-        else:
-            raise EnvironmentNotFound("STAGE")
         
 
     @staticmethod
