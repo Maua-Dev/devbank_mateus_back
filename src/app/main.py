98b43c1b9dd7b0e29427a50981c694854b8a20fc
Matelz <49626198+Matelz@users.noreply.github.com>
2025-05-12 00:22:08 -0300

    hotfix: add route for testing

--- src/app/main.py
+++ src/app/main.py
@@ -16,100 +16,106 @@ account_repo = Environments.get_account_repo()()
 transaction_repo = Environments.get_transaction_repo()()
 transaction_history_repo = Environments.get_transaction_history_repo()()
 
-@app.get("/")
-def get_account_details():
-    account = account_repo.get_account()
-    return account.to_dict()
-
-@app.post("/deposit", status_code=201)
-def deposit(request: dict):
-    bills = request
-
-    validation_bills = Transaction.validate_bills(bills=bills)
-    if not validation_bills[0]:
-        raise HTTPException(status_code=400, detail=validation_bills[1])
-    
-    totalSum = sum([
-        bills[bill] * int(bill) for bill in bills
-    ])
-
-    old_balance = account_repo.get_balance()
-
-    if totalSum > old_balance * 2:
-        raise HTTPException(status_code=403, detail="Depósito suspeito")
-    
-    newTransaction = Transaction(
-        transaction_type=TransactionTypeEnum.DEPOSIT,
-        bills=bills
-    )
-
-    transaction_repo.create_transaction(newTransaction)
-
-    updatedAccount = account_repo.set_balance(
-        old_balance + totalSum
-    )
-
-    transaction_history_repo.create_transaction_history({
-        "type": TransactionTypeEnum.DEPOSIT,
-        "value": totalSum,
-        "current_balance": updatedAccount.current_balance,
-        "timestamp": datetime.now().isoformat(),
-    })
-
+@app.get("/test")
+def test():
     return {
-        "current_balance": updatedAccount.current_balance,
-        "timestamp": datetime.now().isoformat(),
+        "message": "Hello, World!"
     }
 
-@app.post("/withdraw", status_code=201)
-def withdraw(request: dict):
-    bills = request
+# @app.get("/")
+# def get_account_details():
+#     account = account_repo.get_account()
+#     return account.to_dict()
 
-    validation_bills = Transaction.validate_bills(bills=bills)
-    if not validation_bills[0]:
-        raise HTTPException(status_code=400, detail=validation_bills[1])
+# @app.post("/deposit", status_code=201)
+# def deposit(request: dict):
+#     bills = request
+
+#     validation_bills = Transaction.validate_bills(bills=bills)
+#     if not validation_bills[0]:
+#         raise HTTPException(status_code=400, detail=validation_bills[1])
     
-    totalSum = sum([
-        bills[bill] * int(bill) for bill in bills
-    ])
+#     totalSum = sum([
+#         bills[bill] * int(bill) for bill in bills
+#     ])
 
-    old_balance = account_repo.get_balance()
+#     old_balance = account_repo.get_balance()
 
-    if totalSum > old_balance:
-        raise HTTPException(status_code=403, detail="Saque insuficiente para a transação")
+#     if totalSum > old_balance * 2:
+#         raise HTTPException(status_code=403, detail="Depósito suspeito")
     
-    newTransaction = Transaction(
-        transaction_type=TransactionTypeEnum.WITHDRAW,
-        bills=bills
-    )
+#     newTransaction = Transaction(
+#         transaction_type=TransactionTypeEnum.DEPOSIT,
+#         bills=bills
+#     )
 
-    transaction_repo.create_transaction(newTransaction)
+#     transaction_repo.create_transaction(newTransaction)
 
-    updatedAccount = account_repo.set_balance(
-        old_balance - totalSum
-    )
+#     updatedAccount = account_repo.set_balance(
+#         old_balance + totalSum
+#     )
 
-    transaction_history_repo.create_transaction_history({
-        "type": TransactionTypeEnum.WITHDRAW,
-        "value": totalSum,
-        "current_balance": updatedAccount.current_balance,
-        "timestamp": datetime.now().isoformat(),
-    })
+#     transaction_history_repo.create_transaction_history({
+#         "type": TransactionTypeEnum.DEPOSIT,
+#         "value": totalSum,
+#         "current_balance": updatedAccount.current_balance,
+#         "timestamp": datetime.now().isoformat(),
+#     })
 
-    return {
-        "current_balance": updatedAccount.current_balance,
-        "timestamp": datetime.now().isoformat(),
-    }
+#     return {
+#         "current_balance": updatedAccount.current_balance,
+#         "timestamp": datetime.now().isoformat(),
+#     }
 
-@app.get("/history")
-def get_transaction_history():
-    transaction_history = transaction_history_repo.get_transaction_history()
-    print(transaction_history[0].to_dict())
+# @app.post("/withdraw", status_code=201)
+# def withdraw(request: dict):
+#     bills = request
 
-    return {
-        "all_transactions": [
-            transaction for transaction in transaction_history
-        ]
-    }
+#     validation_bills = Transaction.validate_bills(bills=bills)
+#     if not validation_bills[0]:
+#         raise HTTPException(status_code=400, detail=validation_bills[1])
+    
+#     totalSum = sum([
+#         bills[bill] * int(bill) for bill in bills
+#     ])
+
+#     old_balance = account_repo.get_balance()
+
+#     if totalSum > old_balance:
+#         raise HTTPException(status_code=403, detail="Saque insuficiente para a transação")
+    
+#     newTransaction = Transaction(
+#         transaction_type=TransactionTypeEnum.WITHDRAW,
+#         bills=bills
+#     )
+
+#     transaction_repo.create_transaction(newTransaction)
+
+#     updatedAccount = account_repo.set_balance(
+#         old_balance - totalSum
+#     )
+
+#     transaction_history_repo.create_transaction_history({
+#         "type": TransactionTypeEnum.WITHDRAW,
+#         "value": totalSum,
+#         "current_balance": updatedAccount.current_balance,
+#         "timestamp": datetime.now().isoformat(),
+#     })
+
+#     return {
+#         "current_balance": updatedAccount.current_balance,
+#         "timestamp": datetime.now().isoformat(),
+#     }
+
+# @app.get("/history")
+# def get_transaction_history():
+#     transaction_history = transaction_history_repo.get_transaction_history()
+#     print(transaction_history[0].to_dict())
+
+#     return {
+#         "all_transactions": [
+#             transaction for transaction in transaction_history
+#         ]
+#     }
 
 handler = Mangum(app, lifespan="off")
